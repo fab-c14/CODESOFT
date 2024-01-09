32f25b7f520ab5e7442f3eec8d6d638a3b4b73dd
Faisal Bhat <plesim18@gmail.com>
2024-01-09 14:01:08 +0000

    task 3 half done

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import 'tachyons/css/tachyons.min.css';
-import './App.css';
+import 'tachyons';
 
 const Calculator = () => {
   const [input, setInput] = useState('');
@@ -24,166 +23,125 @@ const Calculator = () => {
   };
 
   return (
-    <div>
+    <div className="tc pa3 bg-light-gray br4 v-mid">
+      <div className="grid grid-cols-4 gap-2 ">
       <input
         type="text"
         value={input}
         readOnly
-        className="pa2 input-reset ba b--black-20 br4 mb2"
+        className="pa2 f4 input-reset ba b--black-20 br4 mb2 w-40 tc"
       />
-      <div className="flex justify-between">
+      <div>
         <button
           onClick={() => handleButtonClick('7')}
-          className="pa2 bg-light-gray bn br4 grow"
+          className="ma2 pa3 f3 bg-red bn br4 grow"
         >
           7
         </button>
         <button
           onClick={() => handleButtonClick('8')}
-          className="pa2 bg-light-gray bn br4 grow"
+          className="ma2 pa3 f3 bg-red bn br4 grow"
         >
           8
         </button>
         <button
           onClick={() => handleButtonClick('9')}
-          className="pa2 bg-light-gray bn br4 grow"
+          className="ma2 pa3 f3 bg-red bn br4 grow"
         >
           9
         </button>
         <button
           onClick={() => handleButtonClick('/')}
-          className="pa2 bg-light-gray bn br4 grow"
+          className="ma2 pa3 f3 bg-green bn br4 grow"
         >
           /
         </button>
       </div>
-      <div className="flex justify-between">
+      <div>
         <button
           onClick={() => handleButtonClick('4')}
-          className="pa2 bg-light-gray bn br4 grow"
+          className="ma2 pa3 f3 bg-red bn br4 grow"
         >
           4
         </button>
         <button
           onClick={() => handleButtonClick('5')}
-          className="pa2 bg-light-gray bn br4 grow"
+          className="ma2 pa3 f3 bg-red bn br4 grow"
         >
           5
         </button>
         <button
           onClick={() => handleButtonClick('6')}
-          className="pa2 bg-light-gray bn br4 grow"
+          className="ma2 pa3 f3 bg-red bn br4 grow"
         >
           6
         </button>
+     
         <button
           onClick={() => handleButtonClick('*')}
-          className="pa2 bg-light-gray bn br4 grow"
-        >
+          className="ma2 pa3 f3 bg-green bn br4 grow"
+          >
           *
         </button>
       </div>
-      <div className="flex justify-between">
+      <div>
         <button
           onClick={() => handleButtonClick('1')}
-          className="pa2 bg-light-gray bn br4 grow"
+          className="ma2 pa3 f3 bg-red bn br4 grow"
         >
           1
         </button>
         <button
           onClick={() => handleButtonClick('2')}
-          className="pa2 bg-light-gray bn br4 grow"
+          className="ma2 pa3 f3 bg-red bn br4 grow"
         >
           2
         </button>
         <button
           onClick={() => handleButtonClick('3')}
-          className="pa2 bg-light-gray bn br4 grow"
+          className="ma2 pa3 f3 bg-red bn br4 grow"
         >
           3
         </button>
         <button
           onClick={() => handleButtonClick('-')}
-          className="pa2 bg-light-gray bn br4 grow"
+          className="ma2 pa3 f3 bg-green bn br4 grow"
         >
           -
         </button>
-      </div>
-      <div className="flex justify-between">
+        </div>
+        <div>
+
+        
         <button
           onClick={() => handleButtonClick('0')}
-          className="pa2 bg-light-gray bn br4 grow"
+          className="ma2 pa3 f3 bg-red bn br4 grow"
         >
           0
         </button>
         <button
           onClick={handleClear}
-          className="pa2 bg-light-gray bn br4 grow"
+          className="ma2 pa3 f3 bg-red bn br4 grow"
         >
           C
         </button>
         <button
           onClick={handleCalculate}
-          className="pa2 bg-light-gray bn br4 grow"
+          className="ma2 pa3 f3 bg-green bn br4 grow"
         >
           =
         </button>
         <button
           onClick={() => handleButtonClick('+')}
-          className="pa2 bg-light-gray bn br4 grow"
+          className="ma2 pa3 f3 bg-green bn br4 grow"
         >
           +
         </button>
       </div>
-      {result && <div className="pa3 result">{result}</div>}
+      {result && <div className="pa3 f4 b result">{"Output  : "}{result}</div>}
+      </div> 
     </div>
   );
 };
 
-const App = () => {
-  return (
-    <div className="App tc">
-  {/* <ul class="background">
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-      <li></li>
-    </ul> */}
-      <div className="pa3 bg-light-gray br4">
-        <Calculator />
-      </div>
-    </div>
-  );
-};
-
-export default App;
+export default Calculator;
